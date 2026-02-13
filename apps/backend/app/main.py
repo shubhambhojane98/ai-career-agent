7335fb90997de8512ffcbeaafc52ccc387be4644
shubhambhojane98 <shubhambhojane90@gmail.com>
2026-02-13 21:26:07 +0530

    update cross origin

--- apps/backend/app/main.py
+++ apps/backend/app/main.py
@@ -10,7 +10,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:3000",
         "http://127.0.0.1:3000",
-        "https://ai-career-agent-topaz.vercel.app/",
+        "https://ai-career-agent-topaz.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
